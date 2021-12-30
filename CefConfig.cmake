2c53ea5e2dd4b362b0868eb69583c99337d97e45
Sheen Tian <sheentianshen@gmail.com>
2021-12-30 15:03:57 +0800

    Download cef to different path for different platforms

--- CefConfig.cmake
+++ CefConfig.cmake
@@ -6,27 +6,27 @@ if(OS_WINDOWS)
     # Change this value to swith between different CEF versions.
     "https://cef-builds.spotifycdn.com/cef_binary_89.0.12%2Bg2b76680%2Bchromium-89.0.4389.90_windows64.tar.bz2"
     )
-elseif(OS_MACOS)
-  set(CEF_SDK_URL
-    # Change this value to swith between different CEF versions.
-    "https://cef-builds.spotifycdn.com/cef_binary_89.0.12%2Bg2b76680%2Bchromium-89.0.4389.90_macosx64.tar.bz2"
-    )
 elseif (OS_LINUX)
   set(CEF_SDK_URL
     # Change this value to swith between different CEF versions.
     "https://cef-builds.spotifycdn.com/cef_binary_89.0.12%2Bg2b76680%2Bchromium-89.0.4389.90_linux64.tar.bz2"
     )
+elseif(OS_MACOS)
+  set(CEF_SDK_URL
+    # Change this value to swith between different CEF versions.
+    "https://cef-builds.spotifycdn.com/cef_binary_89.0.12%2Bg2b76680%2Bchromium-89.0.4389.90_macosx64.tar.bz2"
+    )
 else()
   message(FATAL_ERROR "Unsupported plaftorm")
 endif()
-
 
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 # Usually, there is NO NEED to modify the following config
 #
 # Download CEF binary package
 #
-set (CEF_SDK_PACKAGE "${CMAKE_CURRENT_SOURCE_DIR}/dep/cef_package.tar.bz2")
+set(CEF_SDK_WORKSPACE "${CMAKE_CURRENT_BINARY_DIR}/cef")
+set(CEF_SDK_PACKAGE "${CEF_SDK_WORKSPACE}/cef_package.tar.bz2")
 if(NOT EXISTS "${CEF_SDK_PACKAGE}")
   file(DOWNLOAD 
     "${CEF_SDK_URL}"        # URL 
@@ -38,7 +38,7 @@ endif()
 #
 # Extact the CEF SDK dir
 #
-file(GLOB CEF_SDK_DIR "${CMAKE_CURRENT_SOURCE_DIR}/dep/cef_binary_*")
+file(GLOB CEF_SDK_DIR "${CEF_SDK_WORKSPACE}/cef_binary_*")
 
 #
 # Extract CEF binary package
@@ -49,10 +49,10 @@ if (NOT EXISTS ${CEF_SDK_DIR})
   # Extract
   file(ARCHIVE_EXTRACT
     INPUT "${CEF_SDK_PACKAGE}"
-    DESTINATION "${CMAKE_CURRENT_SOURCE_DIR}/dep")
+    DESTINATION "${CEF_SDK_WORKSPACE}/")
 
   # Capture the dir name
-  file(GLOB CEF_SDK_DIR "${CMAKE_CURRENT_SOURCE_DIR}/dep/cef_binary_*")
+  file(GLOB CEF_SDK_DIR "${CEF_SDK_WORKSPACE}/cef_binary_*")
 endif()
 
 # output
