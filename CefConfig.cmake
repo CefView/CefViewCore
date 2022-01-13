0393c149ad9f9b9515034ff2cad7515f61614d5e
Sheen Tian <sheentianshen@gmail.com>
2022-01-13 20:45:33 +0800

    Adjust CEF sdk download location

--- CefConfig.cmake
+++ CefConfig.cmake
@@ -1,58 +1,60 @@
 #
 # The link for downloading the CEF binary sdk 
 #
+set(CEF_SDK_VERSION "cef_binary_89.0.12+g2b76680+chromium-89.0.4389.90")
+
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# Generally, there is NO NEED to modify the following config
+#
+# Download CEF binary package
+#
 if(OS_WINDOWS)
-  set(CEF_SDK_URL
-    # Change this value to swith between different CEF versions.
-    "https://cef-builds.spotifycdn.com/cef_binary_89.0.12%2Bg2b76680%2Bchromium-89.0.4389.90_windows64.tar.bz2"
-    )
+  set(CEF_SDK_PLATFORM "windows64")
 elseif (OS_LINUX)
-  set(CEF_SDK_URL
-    # Change this value to swith between different CEF versions.
-    "https://cef-builds.spotifycdn.com/cef_binary_89.0.12%2Bg2b76680%2Bchromium-89.0.4389.90_linux64.tar.bz2"
-    )
+  set(CEF_SDK_PLATFORM "linux64")
 elseif(OS_MACOS)
-  set(CEF_SDK_URL
-    # Change this value to swith between different CEF versions.
-    "https://cef-builds.spotifycdn.com/cef_binary_89.0.12%2Bg2b76680%2Bchromium-89.0.4389.90_macosx64.tar.bz2"
-    )
+  set(CEF_SDK_PLATFORM "macosx64")
 else()
   message(FATAL_ERROR "Unsupported plaftorm")
 endif()
 
-#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-# Usually, there is NO NEED to modify the following config
-#
-# Download CEF binary package
-#
-set(CEF_SDK_WORKSPACE "${CMAKE_CURRENT_BINARY_DIR}/cef")
-set(CEF_SDK_PACKAGE "${CEF_SDK_WORKSPACE}/cef_package.tar.bz2")
-if(NOT EXISTS "${CEF_SDK_PACKAGE}")
-  file(DOWNLOAD 
-    "${CEF_SDK_URL}"        # URL 
-    "${CEF_SDK_PACKAGE}"    # Local Path
-    SHOW_PROGRESS 
-    TLS_VERIFY ON)
-endif()
+# set cef sdk package name
+set(CEF_SDK_WORKSPACE       "${CMAKE_CURRENT_SOURCE_DIR}/dep")
 
-#
-# Extact the CEF SDK dir
-#
-file(GLOB CEF_SDK_DIR "${CEF_SDK_WORKSPACE}/cef_binary_*")
+set(CEF_SDK_PACKAGE_NAME    "${CEF_SDK_VERSION}_${CEF_SDK_PLATFORM}.tar.bz2")
+set(CEF_SDK_DOWNLOAD_URL    "https://cef-builds.spotifycdn.com/${CEF_SDK_PACKAGE_NAME}")
+set(CEF_SDK_LOCAL_PACKAGE   "${CEF_SDK_WORKSPACE}/${CEF_SDK_PACKAGE_NAME}")
 
-#
+set (CEF_SDK_EXTRACTED_DIR  "${CEF_SDK_WORKSPACE}/${CEF_SDK_VERSION}_${CEF_SDK_PLATFORM}")
+
+message(STATUS "CEF_SDK_DOWNLOAD_URL: ${CEF_SDK_DOWNLOAD_URL}")
+message(STATUS "CEF_SDK_LOCAL_PACKAGE: ${CEF_SDK_LOCAL_PACKAGE}")
+
+# Scan extracted sdk dir
+file(GLOB CEF_SDK_DIR "${CEF_SDK_EXTRACTED_DIR}")
+
 # Extract CEF binary package
-#
 if (NOT EXISTS ${CEF_SDK_DIR})
   message(STATUS "CEF SDK dir does not exist, extracting new one....")
 
+  # if no cef local package then download it
+  if(NOT EXISTS "${CEF_SDK_LOCAL_PACKAGE}")
+    message(STATUS "CEF SDK package not found, start downloading....")
+    file(DOWNLOAD 
+      "${CEF_SDK_DOWNLOAD_URL}"   # URL 
+      "${CEF_SDK_LOCAL_PACKAGE}"  # Local Path
+      SHOW_PROGRESS 
+      TLS_VERIFY ON)
+  endif()
+
   # Extract
   file(ARCHIVE_EXTRACT
-    INPUT "${CEF_SDK_PACKAGE}"
-    DESTINATION "${CEF_SDK_WORKSPACE}/")
+    INPUT "${CEF_SDK_LOCAL_PACKAGE}"
+    DESTINATION "${CEF_SDK_WORKSPACE}"
+  )
 
   # Capture the dir name
-  file(GLOB CEF_SDK_DIR "${CEF_SDK_WORKSPACE}/cef_binary_*")
+  file(GLOB CEF_SDK_DIR "${CEF_SDK_EXTRACTED_DIR}")
 endif()
 
 # output
