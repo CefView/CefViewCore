0b49124be43e82477495f9a38646470171509efe
Sheen Tian Shen <sheentianshen@gmail.com>
2022-06-03 13:23:51 +0800

    Change to weak ptr

--- src/CefView/CefBrowserApp/FaviconDownload/CefViewFaviconDownloadCallback.h
+++ src/CefView/CefBrowserApp/FaviconDownload/CefViewFaviconDownloadCallback.h
@@ -1,7 +1,5 @@
 ﻿#pragma once
 
-#pragma once
-
 #pragma region std_headers
 #include <vector>
 #pragma endregion std_headers
@@ -12,7 +10,6 @@
 #include <include/cef_scheme.h>
 #pragma endregion cef_headers
 
-#include <CefViewCoreProtocol.h>
 #include <CefViewBrowserClientDelegate.h>
 
 class CefViewFaviconDownloadCallback : public CefDownloadImageCallback
@@ -24,12 +21,13 @@ public:
 
   void OnDownloadImageFinished(const CefString& image_url, int http_status_code, CefRefPtr<CefImage> image) override
   {
-    if (image)
-      client_handler_->faviconChanged(image);
+    auto client_handler = client_handler_.lock();
+    if (client_handler && image)
+      client_handler->faviconChanged(image);
   }
 
 private:
-  CefViewBrowserClientDelegateInterface::RefPtr client_handler_;
+  CefViewBrowserClientDelegateInterface::WeakPtr client_handler_;
 
   IMPLEMENT_REFCOUNTING(CefViewFaviconDownloadCallback);
   DISALLOW_COPY_AND_ASSIGN(CefViewFaviconDownloadCallback);
