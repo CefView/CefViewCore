c0a8bf845f47bdd344975c361750e7a9387330da
HQND\Administrator <leashi@qq.com>
2022-07-30 16:48:46 +0800

    添加响应数据拦截接口

--- include/CefViewBrowserClient.h
+++ include/CefViewBrowserClient.h
@@ -142,6 +142,20 @@ public:
                              const CefString& url,
                              int64_t context);
 
+  /// <summary>
+  ///
+  /// </summary>
+  /// <param name="browser"></param>
+  /// <param name="frame"></param>
+  /// <param name="request"></param>
+  /// <param name="response"></param>
+  /// <returns></returns>
+  virtual CefRefPtr<CefResponseFilter> GetResourceResponseFilter(CefRefPtr<CefBrowser> browser,
+                                                                 CefRefPtr<CefFrame> frame,
+                                                                 CefRefPtr<CefRequest> request,
+                                                                 CefRefPtr<CefResponse> response) override;
+
+
 protected:
   bool DispatchRenderMessage(CefRefPtr<CefBrowser> browser,
                              CefRefPtr<CefFrame> frame,
@@ -351,6 +365,13 @@ protected:
                                                            CefRefPtr<CefFrame> frame,
                                                            CefRefPtr<CefRequest> request) override;
 
+  virtual void OnResourceLoadComplete(CefRefPtr<CefBrowser> browser,
+                                      CefRefPtr<CefFrame> frame,
+                                      CefRefPtr<CefRequest> request,
+                                      CefRefPtr<CefResponse> response,
+                                      URLRequestStatus status,
+                                      int64 received_content_length) override;
+
   virtual void OnProtocolExecution(CefRefPtr<CefBrowser> browser,
                                    CefRefPtr<CefFrame> frame,
                                    CefRefPtr<CefRequest> request,
@@ -358,6 +379,7 @@ protected:
 
   // CefDownloadHandler
   virtual CefRefPtr<CefDownloadHandler> GetDownloadHandler() override;
+  
   void OnBeforeDownload(CefRefPtr<CefBrowser> browser,
                         CefRefPtr<CefDownloadItem> download_item,
                         const CefString& suggested_name,
